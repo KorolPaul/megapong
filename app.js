7162428bdc352a3669d260463abaafd2b3984aaa
KorolPaul <korol.paul@gmail.com>
2014-05-12 16:57:45 +0300

    17

--- app.js
+++ app.js
@@ -17,3 +17,13 @@ server.on('connection', function (ws) {
 });
 
 console.log("Server started at 8080");
+
+var express = require('express'),
+        app = express();
+
+app.get('/', function (req, res) {
+    console.log('Request received: ' + req.url);
+    res.send('Hello World!');
+});
+
+app.listen('8081');
