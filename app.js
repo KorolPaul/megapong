254974bd710bb796674d3d21d6b57c86118dfc9e
KorolPaul <korol.paul@gmail.com>
2014-05-13 14:08:29 +0300

    23

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-var http = require('http');
-var WebSocketServer = new require('ws'),
-    server = new WebSocketServer.Server({ port: 8081 });
+var http = require('http').createServer(handler),
+    server = require('socket.io').listen(http);
+
+http.listen(8081);
+/*var WebSocketServer = new require('ws'),
+    server = new WebSocketServer.Server({ port: 8081 });*/
 
 var players = [];
 
-server.on('connection', function (ws) {
+server.sockets.on('connection', function (ws) {
     players[players.length++] = ws;
     console.log("new connection");
 
-    ws.on('message', function(message) {
+    ws.on('message', function (message) {
         console.log(message);
 
         for (key in players)
-            players[key].send(message);
+            players[key].emit('message', message);
     });
 });
 
 console.log("Server started at 8080");
 
-var express = require('express'),
-        app = express();
-app.get('/', function (req, res) {
-    console.log('Request received: ' + req.url);
-    res.send('Hello World!');
-});
-
-
-app.listen('8080');
+function handler(req, res) {
+    res.writeHead(200);
+    res.end(data);
+}
