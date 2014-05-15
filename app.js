55a1f5fe1378acd3970ba9ee745d43c875a5a1ee
KorolPaul <korol.paul@gmail.com>
2014-05-15 19:46:31 +0300

    49

--- app.js
+++ app.js
@@ -11,13 +11,17 @@ app.listen(parseInt(process.env.PORT));
 console.log('Modulus demo app started on port 8080');
 
 
-var http = require('http'),
-    server = require('socket.io').listen(http);
-http.listen(parseInt(process.env.PORT));
+var http = require('http');
+var httpServer = http.createServer(function (request, response) {
+    request.addListener('end', function () {
+        clientFiles.serve(request, response);
+    });
+}).listen(parseInt(process.env.PORT));
+//http.listen(parseInt(process.env.PORT));
 
 var players = [];
-
-server.sockets.on('connection', function (ws) {
+var webSocket = socketIO.listen(httpServer);
+webSocket.on('connection', function (ws) {
     players[players.length++] = ws;
     console.log("new connection");
 
