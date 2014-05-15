e89a37eeaeb3db5ed67203901cbba544d2e0a153
KorolPaul <korol.paul@gmail.com>
2014-05-15 19:25:06 +0300

    44

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ app.get('/', function (req, res) {
 });
 
 
-app.listen(80);
+app.listen(8080);
 console.log('Modulus demo app started on port 8080');
 
-/*
+
 var http = require('http'),
     server = require('socket.io').listen(http);
-http.listen(8081);
+http.listen(8080);
 
 var players = [];
 
@@ -28,4 +28,4 @@ server.sockets.on('connection', function (ws) {
             players[key].emit('message', message);
     });
 });
-*/
+
