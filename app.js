714d8a1a43c7a31f542f7ca94141e9826cca1e62
KorolPaul <korol.paul@gmail.com>
2014-05-15 16:47:29 +0300

    30

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-var http = require('http'),
-    server = require('socket.io').listen(http);
-
 var express = require('express'),
         app = express();
 
@@ -9,8 +6,13 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+
 app.listen('8080');
-/*http.listen(8081);
+console.log('Modulus demo app started on port 8080');
+/*
+var http = require('http'),
+    server = require('socket.io').listen(http);
+http.listen(8081);
 
 var players = [];
 
